2493e6d4c56402199f3082f1febb7f8847a3bd54
jesus <jcd010795@gmail.com>
2025-11-30 12:38:41 -0600

    Improve blog management with search, display, and filter functions
    Customizations were applied to the post and category management interface to improve usability: - A search function based on post titles and content has been implemented. - A custom display function has been added for categories (post_categories) and renamed using the short_description property for clarity. - Filter lists (list_filter) have been configured for quick navigation and data segmentation.

--- blog/admin.py
+++ blog/admin.py
@@ -6,15 +6,23 @@ from .models import Category, Post
 class CategoryModelAdmin(admin.ModelAdmin):
     list_display = ["name"]
     readonly_fields = ("created", "updated")
+    ordering = ("name",)
 
 
 class PostModelAdmin(admin.ModelAdmin):
-    list_display = [
-        "title",
-        "published",
-        "author",
-    ]
+    list_display = ("title", "published", "author", "post_categories")
     readonly_fields = ("created", "updated")
+    ordering = ("-published",)
+    search_fields = ("title", "content")
+    list_filter = ("author__username", "categories__name")
+
+    # Logica pra mostrar las categorias de cada post
+    def post_categories(self, obj):
+        return ", ".join([c.name for c in obj.categories.all()])
+
+    # Renombrando el metodo para que en el admin se muestre "Categorias"
+    # La invocacion en la list_display conserva el nombre de la funcion
+    post_categories.short_description = "Categorias"
 
 
 admin.site.register(Post, PostModelAdmin)
