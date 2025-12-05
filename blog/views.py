ae40c4bc2bffada22838cffbf6ac1445a0c4bdab
jesus <jcd010795@gmail.com>
2025-12-04 22:50:02 -0600

    Implementation of blog post template
    Creation of template to display blog posts. - Implementation of loop to dynamically display the categories of each post, using the forloop.last logic to add separators between categories conditionally. - Short date format (date: SHORT_DATE_FORMAT) - Use of the linebreaks filter to correctly represent paragraph breaks in the post content

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.core import serializers
-from blog.models import Post
+from .models import Category, Post
 
 
 # Create your views here.
 def list_posts(request):
     posts = Post.objects.all()
     return render(request, "blog/blog.html", {"posts": posts})
+
+
+def list_categories(request):
+    categories = Category.objects.all()
+    return render(request, "blog/categories.html", {"categories": categories})
